a8c55083aac9cee76f66322ec9d9412d30396073
s3851481 <66899593+s3851481@users.noreply.github.com>
2023-03-13 13:18:56 +1000

    Added 10 features for minimum viable product

--- service-desk.java
+++ service-desk.java
@@ -1,3 +1,18 @@
+/* Ticket-System
+    Features include: 
+1. Account setup Feature
+2. Login Feature
+3. Forgotten/reset password Feature
+4. Submit ticket Feature
+5. Ticket Status Feature
+6. Assign Ticket Feature
+7. Technician Login Feature (?not sure if need)
+8. Change Ticket Severity Feature
+9. Change Ticket Status Feature
+10. Archive Ticket Feature
+ */
+
+
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World");
