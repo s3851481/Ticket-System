6d6811b4d8728ec994382e4273d5dbc7bb3409c5
s3851481 <66899593+s3851481@users.noreply.github.com>
2023-03-16 12:51:34 +1000

    created Feature 1 - account setup

--- service-desk.java
+++ service-desk.java
@@ -13,8 +13,57 @@
  */
 
 
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AccountSetup {
+
+
+   private String password;
+   private String email;
+   private String username; 
+   public AccountSetup(String username, String password, String email) {
+      this.password = password;
+      this.email = email;
+      this.username = username;
+   }
+
+   public static void main(String[] args) {
+      try (Scanner scanner = new Scanner(System.in)) {
+         System.out.print("Enter username: ");
+         String username = scanner.nextLine();
+         System.out.print("Enter password: ");
+         String password = scanner.nextLine();
+         System.out.print("Enter email: ");
+         String email = scanner.nextLine();
+
+         AccountSetup account = new AccountSetup(username, password, email);
+         saveAccountToFile(account);
+      
+
+      }
+   }
+   public static void saveAccountToFile(AccountSetup account) {
+    try {
+       FileWriter writer = new FileWriter("accounts.txt", true);
+       writer.write(AccountSetup.getUsername() + "," + AccountSetup.getPassword() + "," + AccountSetup.getEmail() + "\n");
+       writer.close();
+    } catch (IOException e) {
+       e.printStackTrace();
     }
-  }
+ }
+
+private static String getEmail() {
+    return null;
+}
+
+private static String getPassword() {
+    return null;
+}
+
+private static String getUsername() {
+    return null;
+}
+ 
+}
